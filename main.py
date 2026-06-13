0214485921ad04a9ee2736484308dd98602d100d
Jinia Goel <jiniagoel12@gmail.com>
2026-06-13 11:15:21 +0530

    view function added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import json
 app = FastAPI()
 
 @app.get("/")
@@ -8,4 +9,21 @@ def read_root():
 @app.get("/about")
 def read_about():
     return {"About": "This is a simple FastAPI application."}
-    
+
+def load_data():
+    with open("students.json", "r") as file:
+        data = json.load(file)
+    return data
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data 
+
+@app.get("/view/{student_id}")
+def view_student(student_id: int):
+    data = load_data()
+    for student in data:
+        if student["id"] == student_id:
+            return student
+    return {"error":"student not found"}
